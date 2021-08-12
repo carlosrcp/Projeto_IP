1534a4a804f7a320ebc49b4f5b58dfb72dfd4256
Carlos <kbrown_carlos@hotmail.com>
2021-08-12 11:56:09 -0300

    teste 3

--- test.py
+++ test.py
@@ -1,8 +1,25 @@
 #teste de commit
 import pygame
+from sys import exit
 
-pygame.init
-#fim do teste
+pygame.init()
+
+screen_height = 360
+screen_width = 640
+
+screen = pygame.display.set_mode((screen_width,screen_height))
+pygame.display.set_caption("nome teste")
+
+# loop principal
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+            
+    pygame.display.update()
+gig
+    
 
 
-#novo
+#fim do teste 2
