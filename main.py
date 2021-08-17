170c4bc30cf3e922072da5f1a10a6105de147aa1
Carlos <crcp@cin.ufpe.br>
2021-08-17 15:17:46 -0300

    limitando o movimento do nave na tela e alguns comentários

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 from sys import exit
 from pygame import draw
 from pygame import key
-from pygame.constants import KEYDOWN
+from pygame.constants import FULLSCREEN, KEYDOWN, RESIZABLE
 from pygame.display import update
 
 from pygame.mixer import fadeout
@@ -17,41 +17,40 @@ screen_width = 640
 # framerate maximo da janela
 max_fps = 60
 
+# cria a tela
 screen = pygame.display.set_mode((screen_width,screen_height))
+# o nome da janela
 pygame.display.set_caption("nome teste")
+# cria o clock que regula o framerate
 clock = pygame.time.Clock()
 
 # surface do objeto que se move
 test_surface = pygame.Surface((16,16))
 test_surface.fill('red')
 
-# surface do fundo
+# surface do fundo, tem o tamanho da tela e é toda preta
 bg_surface = pygame.Surface((screen_width,screen_height))
 bg_surface.fill('black')
-
-#img_nave = pygame.image.load("nave.png")
 
 # posição inicial
 obj_pos = (640 / 2,360 / 2)
 
-# guardar se o botao está sendo segurado
-move_up = False
-move_down = False
-move_left = False
-move_right = False
-
+# função para desenhar o background, coisas a mais como estrelas ao fundo devem ser adicionados aqui
 def draw_bg():
     screen.blit(bg_surface, (0,0))
 
+
+# classe do projetil
 class Projectile (pygame.sprite.Sprite):
     def __init__(self, x, y) -> None:
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("projectile.png")
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
+
+        # se shot for falso, significa que o projetil está inativo e pode ser disparado
         self.shot = False
     
-    #ready = False
     def update(self):
         
         # se o projetil tiver sido disparado
@@ -59,18 +58,23 @@ class Projectile (pygame.sprite.Sprite):
             speed = 8
             self.rect.y -= speed
         
+        # se o projetil sair da tela, ficar inativo
         if self.rect.y < - 16:
             self.shot = False
         
 
+# grupo que guarda os projeteis a ser disparados
 projectile_group = pygame.sprite.Group()
-for i in range(3):
-    new_projectile = Projectile(0,0)
+for i in range(3): # em range(X), x é o número de projeteis
+    # projetil é criado fora da tela e adicionado ao grupo
+    new_projectile = Projectile(0,-10)
 
     projectile_group.add(new_projectile)
 
+
+# classe da nave do jogador 
 class Player (pygame.sprite.Sprite):
-        
+    
     def __init__(self, x, y) -> None:
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("nave.png")
@@ -79,6 +83,8 @@ class Player (pygame.sprite.Sprite):
 
         self.trigger = False
     
+
+    # atirar se houve projetile disponivel
     def shoot(self):
         for i in projectile_group:
             if i.shot == False:
@@ -87,27 +93,29 @@ class Player (pygame.sprite.Sprite):
                 i.shot = True
                 return
 
+    # update que é chamado a cada frame
     def update(self):
-        
-        speed = 6
 
+        # velocidade da nave, precisa ser ajustada em diagonais        
+        speed = 4
+
+        # checa e inputs e faz o movimento
         key_pressed = pygame.key.get_pressed()
         
-        
-        #key_down = pygame.KEYDOWN()
-
         if key_pressed[pygame.K_LEFT]:
-            self.rect.x -= speed 
+            self.rect.x = max(self.rect.x - speed, 0)  #self.rect.x -= speed 
 
         if key_pressed[pygame.K_RIGHT]:
-            self.rect.x += speed 
+            self.rect.x = min(self.rect.x + speed, screen_width - 16)
         
         if key_pressed[pygame.K_UP]:
-            self.rect.y -= speed 
+            self.rect.y = max(self.rect.y - speed, 0)
 
         if key_pressed[pygame.K_DOWN]:
-            self.rect.y += speed 
+            self.rect.y = min(self.rect.y + speed, screen_height - 16) 
         
+
+        # checa e dispara os projeteis
         if key_pressed[pygame.K_SPACE]:
             if not(self.trigger):
                 self.shoot()
@@ -116,9 +124,11 @@ class Player (pygame.sprite.Sprite):
         else:
             self.trigger = False
 
+
+# grupo dos jogadores, só tem 1
 player_group = pygame.sprite.Group()
 
-
+# cria o jogador na posição inicial
 player = Player(int(screen_width/2), int(screen_height/2))
 player_group.add(player)
 
@@ -129,23 +139,24 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-            
     
+    # dt é o delta time, o tempo de um tick para o outro
     dt = clock.tick(max_fps)
     
+    # update nos projeteis e jogador
+    player_group.update()
+    projectile_group.update()
+    
+
+    # as chamadas de draw devem ser feitas de trás pra frente
+    # começando pelo fundo e terminando pelo jogador
+
+    # desenha o fundo (tela preta)
     draw_bg()
 
-    # desenhar os sprites
-    player_group.update()
+    # desenha os projeteis e o jogador
+    projectile_group.draw(screen)    
     player_group.draw(screen)
-
-    projectile_group.update()
-    projectile_group.draw(screen)
-
 
     pygame.display.update()
 gig
-    
-
-
-#fim do teste 2
